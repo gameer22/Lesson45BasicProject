833ded844c2c43cb33340c48d802dee22e88d6d0
ITSTEP\st <st>
2024-02-29 18:18:37 +0300

    реализовал функцию main

--- Task01/main.cpp
+++ Task01/main.cpp
@@ -4,7 +4,16 @@
 using namespace std;
 
 int main() {
+	int size;
+	
+	do {
+		cout << "input size: ";
+		cin >> size;
+	} while (size <= 0 );
 
+	float* vector = new float[size];
+
+	delete[] vector;
 
 	return 0;
 }
